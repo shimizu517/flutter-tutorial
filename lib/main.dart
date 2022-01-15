b6f786491c17fd3bbbc802fb5041362bb794946d
yuta <namtok.dev@gmail.com>
2022-01-15 11:41:52 +0900

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -39,13 +35,7 @@ class MyApp extends StatelessWidget {
               ),
             ],
           )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text(
-            '41',
-          ),
+          const FavoriteWidget(),
         ],
       ),
     );
@@ -159,5 +149,53 @@ class _RandomWordsState extends State<RandomWords> {
         style: _biggerFont,
       ),
     );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+      } else {
+        _favoriteCount += 1;
+      }
+      _isFavorited = !_isFavorited;
+    });
   }
 }
